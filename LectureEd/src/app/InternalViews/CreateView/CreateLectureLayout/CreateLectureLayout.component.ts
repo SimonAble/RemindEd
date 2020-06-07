5b5aa3cb07b9add1da9c6102f7710170ee2a5466
SimonAble <simonbull1@gmail.com>
2020-06-07 11:27:06 -0400

    Added Lecture Creation Components, removed unecessary subfolders for Learn Components

---
+++ LectureEd/src/app/InternalViews/CreateView/CreateLectureLayout/CreateLectureLayout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-CreateLectureLayout',
+  templateUrl: './CreateLectureLayout.component.html',
+  styleUrls: ['./CreateLectureLayout.component.css']
+})
+export class CreateLectureLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
