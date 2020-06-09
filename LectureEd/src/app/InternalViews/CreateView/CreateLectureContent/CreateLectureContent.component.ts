230ba73662a1306214190b088265d5698491e113
SimonAble <simonbull1@gmail.com>
2020-06-08 21:34:05 -0400

    Added Drag and Drop to Topic Navigation

--- LectureEd/src/app/InternalViews/CreateView/CreateLectureContent/CreateLectureContent.component.ts
+++ LectureEd/src/app/InternalViews/CreateView/CreateLectureContent/CreateLectureContent.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { LectureNavigationModel } from './CreateLectureContent.model';
+import { LectureNavigationModel, LectureTopic } from './CreateLectureContent.model';
 import { CreateLectureContentService } from './CreateLectureContent.service';
 import { FormControl } from '@angular/forms';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-CreateLectureContent',
@@ -13,6 +14,8 @@ export class CreateLectureContentComponent implements OnInit {
   public navTopicModel: LectureNavigationModel;
   public showDelay = new FormControl(500);
   public hideDelay = new FormControl(500);
+  public addNewNavTopic: boolean = false;
+  public navTopicItem: string;
 
   constructor(private lectureContentService: CreateLectureContentService) { }
 
@@ -22,6 +25,22 @@ export class CreateLectureContentComponent implements OnInit {
 
   public getLectureNavigationTopics() {
     this.navTopicModel = this.lectureContentService.getNavigationTopics();
+  }
+
+  public drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.navTopicModel.lectureTopics, event.previousIndex, event.currentIndex);
+  }
+
+  public toggleNavItemInput() {
+    this.addNewNavTopic = !this.addNewNavTopic;
+  }
+
+  public addNewNavTopicItem() {
+    if(this.navTopicItem !== "") {
+      this.navTopicModel.lectureTopics.push(new LectureTopic(this.navTopicItem));
+      this.navTopicItem = "";
+      this.addNewNavTopic = false;
+    }
   }
 
 }
