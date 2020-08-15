9224276839b4b9b06ad6ef0395ed414c22760fd0
SimonAble <simonbull1@gmail.com>
2020-08-14 20:04:17 -0400

    Added Migration Seeding and Extended User Class

--- RemindEd.API/Data/DataContext.cs
+++ RemindEd.API/Data/DataContext.cs
@@ -9,6 +9,7 @@ namespace RemindEd.API.Data
 
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Lecture> Lectures { get; set; }
         public DbSet<Topic> Topics { get; set; }
---
+++ RemindEd.API/Models/Photo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RemindEd.API.Models
+{
+    public class Photo
+    {
+        public int ID { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public bool IsProfilePicture { get; set; }
+        public bool IsProfileBackground { get; set; }
+        public DateTime DateAdded { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
